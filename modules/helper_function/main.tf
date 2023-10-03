f8dcd6c3a93d6e67a49ce1651fd634e424372f33
apankratiev-syberry <82646265+apankratiev-syberry@users.noreply.github.com>
2023-10-03 14:50:06 +0300

    Update main.tf

--- modules/helper_function/main.tf
+++ modules/helper_function/main.tf
@@ -1,6 +1,6 @@
 module "helper_lambda" {
   source  = "Adaptavist/aws-lambda/module"
-  version = "1.8.0"
+  version = "1.34.0"
 
   description                        = "SSM helper for dynamoDB replication state machine"
   function_name                      = "helper-${var.target_account}-${var.target_region}-${var.target_dynamodb_table_name}"
