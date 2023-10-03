6db4c7d5e6aaebd28c7164e0ba8c6119f0e4a3da
apankratiev-syberry <82646265+apankratiev-syberry@users.noreply.github.com>
2023-10-03 15:11:42 +0300

    Update main.tf

--- modules/helper_function/main.tf
+++ modules/helper_function/main.tf
@@ -3,7 +3,7 @@ module "helper_lambda" {
   version = "1.34.0"
   aws_region = "us-east-1"
   description                        = "SSM helper for dynamoDB replication state machine"
-  function_name                      = "helper-${var.target_account}-${var.target_region}-${var.target_dynamodb_table_name}"
+  function_name                      = "mg-${var.target_dynamodb_table_name}"
   disable_label_function_name_prefix = true
   lambda_code_dir                    = "${path.module}/function"
   handler                            = "Helper.lambda_handler"
